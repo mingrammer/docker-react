11b3ffb9a1d47821dc72754ee07eeaca7dc2646d
mingrammer <mingrammer@gmail.com>
2019-12-11 02:46:48 +0900

    Update test

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/i was changed on the feature branch/i);
   expect(linkElement).toBeInTheDocument();
 });
